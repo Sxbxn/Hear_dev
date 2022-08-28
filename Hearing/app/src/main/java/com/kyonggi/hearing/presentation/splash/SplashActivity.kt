59fbf494e31c98b4601602f2d3e86e47930c13f9
Joonhoo <mnc5@naver.com>
2022-08-28 20:40:36 +0900

    FEAT: SplashActivity 추가

---
+++ Hearing/app/src/main/java/com/kyonggi/hearing/presentation/splash/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.kyonggi.hearing.presentation.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.kyonggi.hearing.MainActivity
+import com.kyonggi.hearing.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        goToMain()
+    }
+
+    private fun goToMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(intent)
+            finish()
+        }, DURATION)
+    }
+
+    companion object {
+        const val DURATION = 2000L
+    }
+}
+
