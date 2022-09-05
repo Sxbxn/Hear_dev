42ee22396c479bc8ad4ed12bd1142004385f3bfb
Joonhoo <mnc5@naver.com>
2022-09-05 17:04:21 +0900

    FEAT: video slide animation 구현

--- Hearing/app/src/main/java/com/kyonggi/hearing/presentation/video/VideoActivity.kt
+++ Hearing/app/src/main/java/com/kyonggi/hearing/presentation/video/VideoActivity.kt
@@ -5,14 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import androidx.lifecycle.VIEW_MODEL_STORE_OWNER_KEY
+import android.view.animation.AnimationUtils
 import com.kyonggi.hearing.R
 import com.kyonggi.hearing.databinding.ActivityVideoBinding
 
 class VideoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityVideoBinding
+    // view의 open 여부(테스트용)
+    private var open = true
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,17 +44,38 @@ class VideoActivity : AppCompatActivity() {
         binding.root.setOnTouchListener { _, motionEvent ->
             when (motionEvent.action) {
                 MotionEvent.ACTION_DOWN -> {
-                    if (binding.bottomControlsLayout.visibility == View.GONE) {
-                        binding.bottomControlsLayout.visibility = View.VISIBLE
-                        binding.topControlsLayout.visibility = View.VISIBLE
-
-                    } else {
-                        binding.bottomControlsLayout.visibility = View.GONE
-                        binding.topControlsLayout.visibility = View.GONE
-                    }
+                    setSlideUpAndDown()
                 }
             }
             true
+        }
+    }
+
+    // View visibility
+    private fun setSlideUpAndDown() {
+        val bottom_slideUp = AnimationUtils.loadAnimation(this, R.anim.bottom_slide_up)
+        val bottom_slideDown = AnimationUtils.loadAnimation(this, R.anim.bottom_slide_down)
+
+        val top_slideUp = AnimationUtils.loadAnimation(this, R.anim.top_slide_up)
+        val top_slideDown = AnimationUtils.loadAnimation(this, R.anim.top_slide_down)
+        with(binding) {
+            if (open) {
+                // Top layout
+                topControlsLayout.startAnimation(top_slideUp)
+                topControlsLayout.visibility = View.GONE
+                // Bottom layout
+                bottomControlsLayout.startAnimation(bottom_slideDown)
+                bottomControlsLayout.visibility = View.GONE
+                open = false
+            } else {
+                // Top layout
+                topControlsLayout.startAnimation(top_slideDown)
+                topControlsLayout.visibility = View.VISIBLE
+                // Bottom layout
+                bottomControlsLayout.startAnimation(bottom_slideUp)
+                bottomControlsLayout.visibility = View.VISIBLE
+                open = true
+            }
         }
     }
 }
