1338a96da9d5f869e933b495cb4ae7d58b644707
Subin Jo <sbjo1202@naver.com>
2022-09-09 16:46:51 +0900

    FEAT: 메인페이지 수정중

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,32 +1,19 @@
-// const start = document.querySelector(".start");
+// 닉네임 입력 받기 기능
+const nickname = document.querySelector("#nickname");
+
+const inputNick = nickname.querySelector(".input_nick");
+const buttonNick = nickname.querySelector(".enter_nick");
+
+function nickBtnClick() {
+  console.log("hello", inputNick);
+}
+
+buttonNick.addEventListener("click", nickBtnClick);
+
+
+
+// 오른쪽 상단, 회의 코드 입력 기능
 const join_btn = document.querySelector(".button.btnBorder.btnLightGray");
-// join_btn.onclick = function(event) {
-
-//   start.style.marginLeft = "66%";
-
-//   join_btn.style.display = "none";
-
-//   let f = document.createElement('form');
-//   f.setAttribute('class', 'join_meet1');
-
-//   let i = document.createElement('input');
-//   i.setAttribute('type', 'text');
-//   i.setAttribute('placeholder', '회의 코드 입력');
-//   i.setAttribute('required', 'required');
-//   i.setAttribute('class', 'input_code');
-
-//   // let b = document.createElement('button');
-//   // b.setAttribute('class', 'code_join1');
-
-//   let sp = document.createElement('span');
-//   sp.setAttribute('class', 'material-icons');
-//   sp.innerText = "check_circle_outline"
-
-//   start.prepend(f);
-//   f.appendChild(i);
-//   // f.appendChild(b);
-//   b.appendChild(sp);
-// }
 
 join_btn.onclick = function(event) {
   let inputString = prompt('회의 참여 코드를 입력하세요', 'abcd-100');
